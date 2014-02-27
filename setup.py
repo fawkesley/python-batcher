ed70660d83c376cb65f8491bd2f7abc5a667c9ed
Paul M Furley <paul@paulfurley.com>
2014-02-27 20:11:57 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+long_desc = """
+Process items in batches rather than one-by-one.
+"""
+# See https://pypi.python.org/pypi?%3Aaction=list_classifiers for classifiers
+
+setup(
+    name='batcher',
+    version='0.0.1',
+    description="Process items in batches rather than one-by-one.",
+    long_description=long_desc,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+    ],
+    keywords='batch,database insert',
+    author='Paul M Furley',
+    author_email='paul@paulfurley.com',
+    url='http://paulfurley.com',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+)
